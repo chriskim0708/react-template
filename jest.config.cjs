216bb29a3242263baf7a69100207e56a78c98ec2
chris.kim <chris.kim@wavebridge.com>
2023-01-18 14:51:42 +0900

    add: vite storybook config, vite jest config

--- jest.config.cjs
+++ jest.config.cjs
@@ -2,13 +2,10 @@ module.exports = {
   verbose: false,
   roots: ['<rootDir>/src'],
   transform: {
-    '^.+\\.tsx?$': 'ts-jest',
-  },
-  globals: {
-    'ts-jest': {
-      diagnostics: false,
-    },
+    '^.+\\.tsx?$': 'ts-jest'
   },
   testMatch: ['**/__tests__/**/*.test.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
   collectCoverageFrom: ['**/*.{ts,tsx}', '!**/node_modules/**', '!**/vendor/**'],
 };
+
+
